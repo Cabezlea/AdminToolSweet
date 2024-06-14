ecd2f3cf0cac81cf9409919f4cc21c6f6ef4c8c1
Cabezlea <Leocabezas@icloud.com>
2024-06-14 18:53:00 -0500

    Template table, will proceed to edit as I need it

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,6 +17,42 @@
         <span>Search</span>
       </button>
     </div>
+
+    <!-- Log Table Container -->
+    <div class = "table-container">
+    <table class="content-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Spoonity ID</th>
+            <th>Card Number</th>
+            <th>Document Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Domenic</td>
+            <td>88,110</td>
+            <td>dcode</td>
+          </tr>
+          <tr class="active-row">
+            <td>2</td>
+            <td>Sally</td>
+            <td>72,400</td>
+            <td>Students</td>
+            <td>1234567890</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Nick</td>
+            <td>52,300</td>
+            <td>dcode</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -37,6 +73,8 @@ export default {
 </script>
 
 <style scoped>
+
+/* CSS code for the search bar with the filters */
 
 .home-container {
   width: 100%;
@@ -96,6 +134,47 @@ export default {
 
 .search-input::placeholder {
   color: #ccc;
+}
+
+/* CSS code for the Log Table */
+.content-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    min-width: 400px;
+    border-radius: 5px 5px 0 0;
+    overflow: hidden;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    width: 100%;
+}
+
+.content-table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+    font-weight: bold;
+}
+
+.content-table th,
+.content-table td {
+    padding: 12px 15px;
+}
+
+.content-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.content-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.content-table tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+
+.content-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
 }
 
 
